4fabff19daa0768db72ff8304c3d322a976f5972
CLeith <ciel.github@cielsystems.com>
2018-08-26 20:39:21 -0700

    P2Main: PreProcPipeline started. Undistort and perspective from cal files working. Improved pipeline images display mechanism

--- P2MainNB.py
+++ P2MainNB.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import cv2
 import math
 import os
+import pickle
 
 from moviepy.editor import VideoFileClip
 from IPython.display import HTML
@@ -38,38 +39,7 @@ def GlobalCandidates():
     #    os.makedirs(dirOut)
 
 
-# In[59]:
-
-
-#calFileInNames = glob.glob('camera_cal/cal*.jpg')
-#calFileInNames = ['camera_cal/calibration2.jpg']
-
-def GetImageIteratorFromDir():
-    dirImagesIn = "ImagesIn/TestImagesIn/POVRaw/"
-    fileNameBase = "straight_lines1.jpg"
-
-    #imgInFileNames = [dirImagesIn + "straight_lines1.jpg", dirImagesIn + "straight_lines2.jpg"]
-    imgInFileNames = [dirImagesIn + "straight_lines1.jpg", dirImagesIn + "straight_lines1.jpg"]
-    imgInFileNames = [dirImagesIn + "straight_lines1.jpg"]
-    imageIter = ((mpimg.imread(imgInFileName), imgInFileName) for imgInFileName in imgInFileNames)
-
-    return(imageIter)
-
-
-# In[43]:
-
-
-def ImageProc_Pipeline(imgRaw):
-    dirImagesIn = "ImagesIn/TestImagesIn/PipelineStages/"
-    imgInFileNameBase = "warped_example.jpg"
-    imgInFileName = dirImagesIn + imgInFileNameBase
-
-    imgPipeProc = mpimg.imread(imgInFileName)
-    return imgPipeProc
-    
-
-
-# In[44]:
+# In[6]:
 
 
 def Random_PolyPoints(ploty):
@@ -93,17 +63,18 @@ def Random_PolyPoints(ploty):
     return ploty, leftx
 
 
-# In[46]:
+# In[37]:
 
 
 def Polynomial_Eval(polyCoeff, inVals):
     outVals = self.polyCoeff[0] * inVals**2 + self.polyCoeff[1] * inVals + self.polyCoeff[2]
     return inVals, outVals
 
-def PlotSelectedAndPolynomial(argPolyCoeff, inDomainCoords, xRangeCoords):
-        coA, coB, coC = argPolyCoeff[0],argPolyCoeff[1],argPolyCoeff[2],
+def PlotSelectedAndPolynomialOld(argPolyCoeff, inDomainCoords, xRangeCoords):
+        coA, coB, coC = argPolyCoeff[0], argPolyCoeff[1], argPolyCoeff[2],
         polyEvalOutCoords = coA*inDomainCoords**2 + coB*inDomainCoords + coC
         
+        fig, ax = plt.subplots()      
         plt.plot(xRangeCoords, inDomainCoords, 'o', color='red', markersize=1)
         plt.xlim(0, 1280)
         plt.ylim(0, 720)
@@ -111,8 +82,67 @@ def PlotSelectedAndPolynomial(argPolyCoeff, inDomainCoords, xRangeCoords):
         plt.gca().invert_yaxis() # to visualize as we do the images
         plt.show()
 
+def fig2rgb_array(fig):
+    fig.canvas.draw()
+    buf = fig.canvas.tostring_rgb()
+    ncols, nrows = fig.canvas.get_width_height()
+    return np.fromstring(buf, dtype=np.uint8).reshape(nrows, ncols, 3)
 
-# In[52]:
+def PlotSelectedAndPolynomial(argPolyCoeff, inDomainCoords, xRangeCoords, imgPlotIn):
+        coA, coB, coC = argPolyCoeff[0], argPolyCoeff[1], argPolyCoeff[2],
+        polyEvalOutCoords = coA*inDomainCoords**2 + coB*inDomainCoords + coC
+        
+        fig, ax = plt.subplots()
+      
+        plt.imshow(imgPlotIn)
+        plt.xlim(0, 1280)
+        plt.ylim(0, 720)
+        plt.plot(xRangeCoords, inDomainCoords, 'o', color='red', markersize=1)
+        plt.plot(polyEvalOutCoords, inDomainCoords, color='green', linewidth=2)
+        plt.gca().invert_yaxis() # to visualize as we do the images
+        imgPlotOut = fig2rgb_array(fig)
+        plt.clf()
+        return imgPlotOut
+
+#ImageRecord = namedtuple('Img', 'val left right')
+#def Node(val, left=None, right=None):
+#  return NodeT(val, left, right)
+#        defaultargs = (None, "img_" + str(recIndex), {})
+#        img, imgName, kwargs = tuple(map(lambda x, y: y if y is not None else x, defaultargs, imgRecord))
+
+    
+#-------------------------------------------------------------------
+def PlotImageRecords(imgRecords):
+    fig = plt.gcf()
+    fig.set_size_inches(12,9)
+    #fig.set_dpi(180)
+
+    numImages = len(imgRecords)
+    numCols = 3
+    numRows = math.ceil(numImages/numCols)
+    for recIndex, imgRecord in enumerate(imgRecords):
+        numFields = len(imgRecord)
+        img = imgRecord[0]
+        if (numFields >= 2):
+            imgName =  imgRecord[1]
+        else:
+            imgName =  "img_" + str(recIndex)
+            
+        if (numFields >= 3):
+            kwArgs =  imgRecord[2]
+        else:
+            kwArgs =  {}
+                
+        plt.subplot(numRows, numCols, recIndex+1)
+        plt.title(imgName)
+        #plt.axis('off')
+        plt.imshow(img, **kwArgs)
+        
+    fig.tight_layout()
+    
+
+
+# In[30]:
 
 
 def SelectPixelsUsingSlidingWindow(imgIn, whichLine):    
@@ -150,7 +180,7 @@ def SelectPixelsUsingSlidingWindow(imgIn, whichLine):
     selectedPixelIndicesAccum = []
 
     # Create an output image to draw on for visualizing the result
-    imgOut = np.dstack((imgIn, imgIn, imgIn))
+    imgSelectionOut = np.dstack((imgIn, imgIn, imgIn))
     lineWidth = 3
     colorWinRect = (0,255,0)
     colorNextCenterXLine = (0, 130, 255)
@@ -166,7 +196,7 @@ def SelectPixelsUsingSlidingWindow(imgIn, whichLine):
         win.right = win.centerX +  margin
 
         # Draw the windows on the visualization image
-        cv2.rectangle(imgOut, (win.left, win.bottom), (win.right, win.top), colorWinRect, lineWidth)
+        cv2.rectangle(imgSelectionOut, (win.left, win.bottom), (win.right, win.top), colorWinRect, lineWidth)
 
 
         # Identify the nonzero pixels in x and y within the window 
@@ -191,7 +221,7 @@ def SelectPixelsUsingSlidingWindow(imgIn, whichLine):
 
         Pt0 = (nextWindowCenterX, win.bottom)
         Pt1 = (nextWindowCenterX, win.top)
-        cv2.line(imgOut, Pt0, Pt1, colorNextCenterXLine, lineWidth)
+        cv2.line(imgSelectionOut, Pt0, Pt1, colorNextCenterXLine, lineWidth)
 
  
     # Extract left and right line pixel positions
@@ -199,16 +229,16 @@ def SelectPixelsUsingSlidingWindow(imgIn, whichLine):
     selectedPixelsX = pixelsNon0X[selectedPixelIndices]
     selectedPixelsY = pixelsNon0Y[selectedPixelIndices]
 
-    return selectedPixelsX, selectedPixelsY, imgOut
+    return selectedPixelsX, selectedPixelsY, imgSelectionOut
 
 def Test_SelectPixelsUsingSlidingWindow():
     #%matplotlib qt4
     #%matplotlib inline
-    get_ipython().run_line_magic('matplotlib', 'notebook')
-    selectedPixelsX, selectedPixelsY, imgOut = SelectPixelsUsingSlidingWindow(g_imgTest, "RIGHT")
+    #%matplotlib notebook
+    selectedPixelsX, selectedPixelsY, imgSelectionOut = SelectPixelsUsingSlidingWindow(g_imgTest, "RIGHT")
     plt.figure(figsize=(12,8))
-    imgOut[selectedPixelsY, selectedPixelsX] = [255, 0, 0]
-    plt.imshow(imgOut)
+    imgSelectionOut[selectedPixelsY, selectedPixelsX] = [255, 0, 0]
+    plt.imshow(imgSelectionOut)
     plt.tight_layout()
 
 g_testImgFileName = "ImagesIn/TestImagesIn/PipelineStages/warped_example.jpg"
@@ -216,7 +246,7 @@ g_imgTest = mpimg.imread(g_testImgFileName)
 Test_SelectPixelsUsingSlidingWindow()
 
 
-# In[62]:
+# In[35]:
 
 
 #left_fit = np.array([ 2.13935315e-04, -3.77507980e-01,  4.76902175e+02])
@@ -251,46 +281,162 @@ class CImageLine(object):
         selectedPixelsX = pixelsNon0X[selectedPixelIndices]
         selectedPixelsY = pixelsNon0Y[selectedPixelIndices] 
 
-        return selectedPixelsX, selectedPixelsY
+        return selectedPixelsX, selectedPixelsY, imgSelection
          
     def CalcPolyFit(self, imgIn):
         imageHeight = imgIn.shape[0]
         selectedCoordsY = np.linspace(0, imageHeight-1, num=imageHeight)
 
         if (self.HasPolyFit() == True):
-            selectedPixelsX, selectedPixelsY = self.SelectPixelsUsingPolynomial(self.polyCoeff, imgIn)
+            selectedPixelsX, selectedPixelsY, imgSelection = self.SelectPixelsUsingPolynomial(self.polyCoeff, imgIn)
         else:
             # selectedCoordsY, selectedCoordsX = Random_PolyPoints(selectedCoordsY)
             print("No previous polynomial. Searching with sliding window...")
-            selectedPixelsX, selectedPixelsY, imgSlidingWindows = SelectPixelsUsingSlidingWindow(imgIn, self.name)
+            selectedPixelsX, selectedPixelsY, imgSelection = SelectPixelsUsingSlidingWindow(imgIn, self.name)
 
         polyCoeffNew = np.polyfit(selectedPixelsY, selectedPixelsX, 2)       
-        PlotSelectedAndPolynomial(polyCoeffNew, selectedPixelsY, selectedPixelsX)
         self.polyCoeff = polyCoeffNew
+        
+        imgPlot = imgIn.copy()
+        imgSelection = PlotSelectedAndPolynomial(polyCoeffNew, selectedPixelsY, selectedPixelsX, imgPlot)
 
-get_ipython().run_line_magic('matplotlib', 'notebook')
+        return polyCoeffNew, imgSelection
+    
+#%matplotlib notebook
 #P2Main(g_imageIter)
 
 
-# In[64]:
+# In[39]:
+
+
+#calFileInNames = glob.glob('camera_cal/cal*.jpg')
+#calFileInNames = ['camera_cal/calibration2.jpg']
+
+def GetImageIteratorFromDir():
+    dirImagesIn = "ImagesIn/TestImagesIn/POVRaw/"
+    dirImagesIn = "ImagesIn/VideosIn/project_video_frames/"
+    fileNameBase = "straight_lines1.jpg"
+
+    #imgInFileNames = [dirImagesIn + "straight_lines1.jpg", dirImagesIn + "straight_lines2.jpg"]
+    imgInFileNames = [dirImagesIn + "straight_lines1.jpg", dirImagesIn + "straight_lines1.jpg"]
+    imgInFileNames = [dirImagesIn + "project_video_f1192.jpg"]
+    imageIter = ((mpimg.imread(imgInFileName), imgInFileName) for imgInFileName in imgInFileNames)
+
+    return(imageIter)
+
+
+# In[63]:
+
+
+def CameraCal_Undistort(imgIn, dictCameraCalVals):
+    """
+    Perform image undistortion on a numpy image
+    """
+    imgOut = cv2.undistort(imgIn, dictCameraCalVals["mtx"], dictCameraCalVals["dist"], None, dictCameraCalVals["mtx"])
+    return(imgOut)
+
+def CameraCal_DoPerspectiveTransform(imgIn, matTransform):
+    img_size = (imgIn.shape[1], imgIn.shape[0])
+    interpolation = cv2.INTER_CUBIC
+    imgOut = cv2.warpPerspective(imgIn, matTransform, img_size, flags=interpolation)
+    return imgOut
+
+
+# In[70]:
+
+
+def ImageProc_PreProcPipeline(imgRaw, dictCameraCalVals):
+    imageRecsPreProc = []
+
+    imgUndistort = CameraCal_Undistort(imgRaw, dictCameraCalVals)
+    imageRecsPreProc.append( (imgUndistort, "imgUndistort") ) 
+    
+    imgPerspect = DoPerspectiveTransform(imgUndistort, dictCameraCalVals["warpMatrix"])
+    imageRecsPreProc.append( (imgPerspect, "imgPerspect") ) 
+
+    # Get sample lineFindSrc image
+    dirImagesIn = "ImagesIn/TestImagesIn/PipelineStages/"
+    imgInFileNameBase = "warped_example.jpg"
+    imgInFileName = dirImagesIn + imgInFileNameBase
+    imgLineFindSrc = mpimg.imread(imgInFileName)
+    imageRecsPreProc.append( (imgLineFindSrc, "imgLineFindSrc", {"cmap":"gray"}) )       
+
+    return imageRecsPreProc
+    
+
+
+# In[65]:
+
+
+def GetCameraCalVals(cameraDistortionCalValsFileName, cameraPerspectiveWarpMatrixFileName):
+    dictCameraCalVals = pickle.load( open(cameraDistortionCalValsFileName, "rb" ) )
+    dictCameraCalVals["warpMatrix"] = pickle.load( open(cameraPerspectiveWarpMatrixFileName, "rb" ) )
+    return(dictCameraCalVals)
+
+
+# In[71]:
 
 
 
-def P2Main(rawImgSrcIter):
-
+def P2Main(rawImgSrcIter, dictCameraCalVals):
+    
     imageLines = [CImageLine("LEFT"),CImageLine("RIGHT")]
     for (imgRaw, imgRawName) in rawImgSrcIter:
         print("P2Main.ImgPipeProc: ", imgRawName)
+        imageRecsCurFrame = []
+        imageRecsCurFrame.append( (imgRaw, imgRawName) ) 
         
-        imgPipeProc = ImageProc_Pipeline(imgRawName)
-        
+        imageRecsPreProc = ImageProc_PreProcPipeline(imgRaw, dictCameraCalVals)
+        imageRecsCurFrame.extend(imageRecsPreProc)
+        imgLineFindSrc = imageRecsPreProc[-1][0]
+
         for curImageLine in imageLines :
             print("P2Main.LineProc: ", curImageLine.name)
-            curImageLine.CalcPolyFit(imgPipeProc)
+            polyCoeffNew, imgSelection = curImageLine.CalcPolyFit(imgLineFindSrc)
+            imageRecsCurFrame.append( (imgSelection, "imgSelection_" + curImageLine.name ))       
             
         print(" ")
-        
-# MainInvocation
+
+        doDebugImages = True
+        if doDebugImages :
+            get_ipython().run_line_magic('matplotlib', 'qt4')
+            PlotImageRecords(imageRecsCurFrame)
+            key = cv2.waitKey(2000)
+
+
+#============================= Main Invocation =================
+g_cameraDistortionCalValsFileName = "CameraDistortionCalVals.pypickle"
+g_cameraPerspectiveWarpMatrixFileName = "CameraPerspectiveWarpMatrix.pypickle"
+
+g_dictCameraCalVals = GetCameraCalVals(g_cameraDistortionCalValsFileName, g_cameraPerspectiveWarpMatrixFileName)
 g_imageIter = GetImageIteratorFromDir()
-P2Main(g_imageIter)
+
+#================= P2Main() invocation
+P2Main(g_imageIter, g_dictCameraCalVals)
+
+
+# In[ ]:
+
+
+pipelineImages = [
+    (imgRaw, imgRawName),
+    (imgPipeProc, "imgPipeProc", {"cmap":"gray"}),
+    (imgSelection, "imgSelection", ),      
+
+    #(imgHthr, "imgHthr", {"cmap":"gray"}),
+    #(imgLthr, "imgLthr", {"cmap":"gray"}),
+    #(imgSthr, "imgSthr", {"cmap":"gray"}),      
+
+    #(imgIn, imgInFileName),
+    #(imgHSL, "imgHSL"),
+
+    #(imgSobelThrGradX, "imgSobelThrGradX", {"cmap":"gray"}),
+    #(imgSobelThrGradY, "imgSobelThrGradY", {"cmap":"gray"}),
+    #(imgSobelThrMag, "imgSobelThrMag", {"cmap":"gray"}),
+    #(imgSobelThrDir, "imgSobelThrDir", {"cmap":"gray"}),
+
+    #(imgComboSobel, "imgComboSobel", {"cmap":"gray"}),
+    #(imgComboSatSobelX_Stack, "imgComboSatSobelX_Stack", {"cmap":"gray"}),
+    #(imgComboSatSobelX_Or, "imgComboSatSobelX_Or", {"cmap":"gray"}), 
+]
 
